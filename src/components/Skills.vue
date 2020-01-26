98efc17eed6da8514d5973e48edaa4c859f80b88
Breno Nunes <breno.arantes97@gmail.com>
2020-01-25 21:26:27 -0300

    Add VeeValidate 3.2.2 Add input skill and validate it

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
+  <div class="container">
     <div class="holder">
+      <ValidationObserver v-slot="{ handleSubmit }">
+        <form @submit.prevent="handleSubmit(addSkill)">
+          <ValidationProvider name="skill" rules="min:5|max:10" v-slot="{ errors }">
+            <input type="text" v-model="skill" placeholder="Enter a skill you have" />
+            <p class="alert" v-if="errors[0]">{{errors[0]}}</p>
+          </ValidationProvider>
+        </form>
+      </ValidationObserver>
       <ul>
-        <li v-for="(data, index) in skills" :key="index">
-          {{data.skill}}
-        </li>
+        <li v-for="(data, index) in skills" :key="index">{{data.skill}}</li>
       </ul>
     </div>
 
@@ -13,56 +19,76 @@
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   name: "Skills",
   data() {
     return {
-      skills: [
-        { "skill" : "VueJs" },
-        { "skill" : "Java" }
-      ],
-    }
+      skills: [],
+      skill: ""
+    };
   },
   props: {
     msg: String
   },
   methods: {
-    changeName(){
-      this.name = "Breno Arantes";
+    addSkill() {
+      this.skills.push({ skill: this.skill });
+      this.skill = "";
     }
   }
-}
+};
 </script>
 
 <style scoped>
+.holder {
+  background: #fff;
+}
 
-  .holder {
-    background: #fff;
-  }
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
 
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
-  
-  ul li {
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
-    margin-bottom: 2px;
-    color: #3E5252;
-  }
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
 
-  p {
-    text-align:center;
-    padding: 30px 0;
-    color: gray;
-  }
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
 
-  .container {
-    box-shadow: 0px 0px 40px lightgray;
-  }
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
 
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { extend } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+import { messages } from 'vee-validate/dist/locale/en.json';
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule] // assign message
+  });
+});
 
 Vue.config.productionTip = false
 
